dad21dbe230be751dfe2bbae1cfb409e36681ec0
popsiqle <popsiqle@hotmail.com>
2022-04-27 10:01:32 +0430

    import microservices packagee

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+
+const microserviceOptions = {
+  transport: Transport.NATS,
+  options: {
+    url: 'nats://localhost:4222',
+  }
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  app.listen();
 }
 bootstrap();
